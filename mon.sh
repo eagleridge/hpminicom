0c7bebcd111e7260832ebf917a5ce6fbdf2b78e7
eagle <eagleridge@b.com>
2016-12-16 19:23:41 -0800

    update mon.sh

--- mon.sh
+++ mon.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 cnt=0
+pushcnt=0
 
 while [ $cnt -lt 1000 ]; do
 
@@ -10,17 +11,27 @@ while [ $cnt -lt 1000 ]; do
     diff tmpf1 tmpf2 > /dev/null 2>&1
     rc=$?
     if [ $rc -ne 0 ]; then
+        let 'pushcnt = pushcnt +1'
         echo >> logfile
         echo $dt >> logfile
+        echo cnt $cnt >> logfile
+        echo pushcnt $pushcnt >> logfile
         cat tmpf1 >> logfile
         echo >> logfile
         git commit -am 'update_logfile'
         git push
     fi
 
+    echo
+    echo '======================================'
+        echo cnt $cnt
+        echo pushcnt $pushcnt 
+        echo 
     if [ $cnt -lt 10 ]; then
+        echo sleep 60
         sleep 60
     else
+        echo sleep 600
         sleep 600
     fi
 
